573e32387301149f57ba96fff582bce03d1d28d6
Diegon8 <diegolpr95@gmail.com>
2020-09-07 17:52:23 +0200

    Clening done

---
+++ Cleaning_functions.py
@@ -0,0 +1,13 @@
+def lastc(x):
+    return x[-12:]
+
+def roundamount():
+    return lambda a: round(a/1000000, 1)*100000
+
+def roundamountmonth():
+    return lambda a: round(a/1000000, 3)*100000
+
+def roundscore():
+    return lambda a: round(a/100, 2)
+
+
